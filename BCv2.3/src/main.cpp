4a47c510a3ea6900c3c0f259fee2d690f0eb8766
Stefan Turcu <stefan.turcu23@gmail.com>
2021-04-19 23:48:32 +0300

    new software update

--- BCv2.3/src/main.cpp
+++ BCv2.3/src/main.cpp
@@ -2,10 +2,7 @@
 #include <Servo.h>
 #include <Wire.h>
 #include <SPI.h>
-#include <SD.h>
-
 #include <SimpleKalmanFilter.h>
-
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 #include <Adafruit_BMP280.h>
@@ -13,225 +10,167 @@
 // Kalman Filter
 SimpleKalmanFilter pressureKalmanFilter(1, 1, 0.01);
 
-// Serial output refresh time
-const long SERIAL_REFRESH_TIME = 100;
-long refresh_time;
-float baseline = 0; // baseline pressure
-
 // Sensors accel & barometer
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 Adafruit_BMP280 bmp; // use I2C interface
-Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
-Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
+Adafruit_Sensor * bmp_pressure = bmp.getPressureSensor();
 
 // Useful pins
 Servo Servo1;
 const int servoPin = 9;
 const int buzzer = 5;
-const int SDcard = 4;
 const int ledPin = 10;
 
-int state = 0;
-
-const int ground_idle = 1;
-const int powered_flight = 2;
-const int unpowered_flight = 3;
-const int balistic_descent = 4;
-const int chute_descent = 5;
-const int landing_idle = 6;
+int state = 1;
+const short ground_idle = 1;
+const short powered_flight = 2;
+const short unpowered_flight = 3;
+const short balistic_descent = 4;
+const short chute_descent = 5;
+const short landing_idle = 6;
 
 // Functions
-void initialize_SDcard();
 void initialize_ADXL345();
 void initialize_BMP280();
-
 void error();
 
 // ============================================ SETUP ============================================
 
-void setup()
-{
+void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(buzzer, OUTPUT);
 
   Servo1.attach(servoPin);
-  Servo1.write(35);
+  Servo1.write(30);
 
   Serial.begin(9600);
 
   // user interface -> startup
-  for (int i = 1; i <= 3; i++)
-  {
-    digitalWrite(ledPin, HIGH);
-    tone(buzzer, 1000);
-    delay(250);
-
-    digitalWrite(ledPin, LOW);
-    delay(250);
-
-    digitalWrite(ledPin, HIGH);
-    tone(buzzer, 600);
-    delay(250);
-
-    digitalWrite(ledPin, LOW);
-    delay(250);
+  for (int i = 6; i <= 9; i++) {
+    tone(buzzer, i * 100);
+    delay(100);
   }
 
-  digitalWrite(ledPin, HIGH);
   noTone(buzzer);
   delay(500);
 
-  // initialize_SDcard();
   initialize_ADXL345();
   initialize_BMP280();
 
-  // getting baseline alt
-  for (int i = 1; i <= 5; i++)
-  {
-    baseline = baseline + bmp.readAltitude();
-    delay(100);
-  }
-  baseline = baseline / 5;
-
-  digitalWrite(ledPin, LOW);
+  tone(buzzer, 600);
+  delay(100);
+  tone(buzzer, 1000);
+  delay(100);
+  noTone(buzzer);
 }
 
 // ============================================ LOOP ============================================
 
-void loop()
-{
-  while (1)
-  {
-    Servo1.write(35);
-    delay(2000);
-    Servo1.write(100);
-    delay(1000);
-  }
+int currentRead = 0, counter = 1;
+float estimated_alt_first, estimated_alt_second = 0;
 
+void loop() {
   sensors_event_t event;
-  accel.getEvent(&event);
+  accel.getEvent( & event);
 
-  switch (state)
-  {
-    case ground_idle:
-    {
-      if (event.acceleration.x >= 14)
+  switch (state) {
+    
+    // GROUND_IDLE STATE - the caraft is in a waiting state for a spike in acceleration. the spike will be twice veriffied
+    case ground_idle: {
+      Serial.println(event.acceleration.x);
+      currentRead = event.acceleration.x;
+
+      if (currentRead >= 15) // de ce am pus aici 14.. trb sa verific
       {
-        delay(100);
-        if (event.acceleration.x >= 14)
-        {
+        if (counter == 2) {
+          Serial.println("checked 2 times the x acceleration for a spike in acceleration, going to next state");
           state += 1;
-          // sdcard -> "vehicle launch"
-          // sdcard -> time
+          break;
         }
+        delay(500);
+        counter++;
+      } else {
+        counter = 1;
       }
       break;
     }
 
-    case powered_flight:
-    {
-      if (event.acceleration.x <= 2)
-      {
-        delay(100);
-        if (event.acceleration.x <= 2)
-        {
+    // POWERED_FLIGHT STATE - the rocket is in flight, powered by the motor. the BC will wait and check for the burnout 
+    case powered_flight: {
+      tone(buzzer, 500);
+      if (event.acceleration.x <= 2) {
+        if (counter == 3) {
+          Serial.println("checked 2 times the x acceleration for unpowered flight, going to next state");
           state += 1;
-          // sdcard -> "meco"
-          // sdcard -> time
+          break;
         }
+        delay(500);
+        counter++;
+      } else {
+        counter = 2;
       }
       break;
     }
 
-    case unpowered_flight:
-    {
-      float estimated_alt_first = pressureKalmanFilter.updateEstimate(bmp.readAltitude());
-      delay(500);
-      float estimated_alt_second = pressureKalmanFilter.updateEstimate(bmp.readAltitude());
+    // UNPOWERED_FLIGHT STATE - the rocket reached the burnout. it is in a costing (decelerating) state. waiting and checking for apogee
+    case unpowered_flight: {
+      tone(buzzer, 700);
+      estimated_alt_second = pressureKalmanFilter.updateEstimate(bmp.readAltitude());
 
-      if (estimated_alt_first >= estimated_alt_second)
-      {
-        state += 1;
-        // sdcard -> "passed apogee"
-        // sdcard -> time
+      if (estimated_alt_first > estimated_alt_second) {
+        if (counter == 4 ) {
+          Serial.println("checked 2 times the x acceleration for unpowered flight, going to next state");
+          state += 1;
+          break;
+        }
+        delay(1000);
+        counter++;
+      }
+      else {
+        counter = 3;
+        estimated_alt_first = estimated_alt_second;
       }
       break;
     }
 
-    case balistic_descent:
-    {
+    // BALISTIC_DESCENT STATE - the rocket is descending balistically. waiting for optimal altitude to deploy parachute
+    case balistic_descent: {
+      tone(buzzer, 900);
       float estimated_alt = pressureKalmanFilter.updateEstimate(bmp.readAltitude());
-
-      if (estimated_alt <= 25.0f)
-      {
+      Serial.println(estimated_alt);
+      if (estimated_alt <= 470.0f) {
         state += 1;
-        Servo1.write(45); // deployment angle
-        // sdcard -> "parachute deployed"
-        // sdcard -> time
+        Servo1.write(65); // deployment servo angle
+        delay(100);
+        Servo1.write(30);
+        delay(100);
+        Servo1.write(65); // deployment servo angle
+        delay(100);
+        Servo1.write(30);
+        delay(100);
       }
       break;
     }
 
-    case chute_descent:
-    {
-      // sdcard -> "chute_descent"
-      // sdcard -> time
-
-      break;
+    // CHUTE_DESCENT STATE - the rocket is descending under parachute. waiting to land and to be recovered
+    case chute_descent: {
+      while (1)
+      {
+        tone(buzzer, 600);
+        delay(100);
+        tone(buzzer, 1000);
+        delay(100);
+        noTone(buzzer);
+      }
     }
 
-    case landing_idle:
-    {
-
-      break;
-    }
   }
-
-  // if (millis() > refresh_time) {
-  //   Serial.print(bmp.readAltitude(), 6);
-  //   Serial.print(", ");
-  //   Serial.print(pressureKalmanFilter.updateEstimate(bmp.readAltitude()), 6);
-  //   Serial.println();
-  //   refresh_time=millis()+SERIAL_REFRESH_TIME;
-  // }
-
-  // Kalman Filter Pressure
-
-  // Serial.print(F("Temperature = "));
-  // Serial.print(bmp.readTemperature());
-  // Serial.println(" *C");
-
-  // Serial.print(F("Pressure = "));
-  // Serial.print(bmp.readPressure()/100); //displaying the Pressure in hPa, you can change the unit
-  // Serial.println(" hPa");
-
-  // Serial.print(F("Approx altitude = "));
-  // Serial.print(bmp.readAltitude(1029)); //The "1013.25" is the pressure(hPa) at sea level in day in your region
-  // Serial.println(" m");                    //If you don't know it, modify it until you get your current altitude
-
-  // Serial.println();
-  // delay(2000);
 }
 
-void initialize_SDcard()
-{
-  Serial.print("Initializing SD card...");
-  pinMode(SDcard, OUTPUT);
+// ============================================ FUNCTIONS ============================================
 
-  if (!SD.begin())
-  {
-    Serial.println("Card failed, or not present");
-    error();
-  }
-
-  Serial.println("card initialized.");
-  File dataFile = SD.open("datalog.txt");
-}
-
-void initialize_ADXL345()
-{
-  if (!accel.begin())
-  {
+void initialize_ADXL345() {
+  if (!accel.begin()) {
     Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
     error();
   }
@@ -240,30 +179,25 @@ void initialize_ADXL345()
   accel.printSensorDetails();
 }
 
-void initialize_BMP280()
-{
+void initialize_BMP280() {
   // barometer sensor setup
-
   Serial.println(F("BMP280 Sensor event test"));
-  if (!bmp.begin(0x76))
-  {
+  if (!bmp.begin(0x76)) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     error();
   }
 
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
-                  Adafruit_BMP280::SAMPLING_X2,
-                  Adafruit_BMP280::SAMPLING_X16,
-                  Adafruit_BMP280::FILTER_X16,
-                  Adafruit_BMP280::STANDBY_MS_500);
+    Adafruit_BMP280::SAMPLING_X2,
+    Adafruit_BMP280::SAMPLING_X16,
+    Adafruit_BMP280::FILTER_X16,
+    Adafruit_BMP280::STANDBY_MS_500);
 
-  bmp_temp->printSensorDetails();
+  bmp_pressure -> printSensorDetails();
 }
 
-void error()
-{
-  while (1)
-  {
+void error() {
+  while (1) {
     digitalWrite(ledPin, HIGH);
     tone(buzzer, 800);
     delay(100);
